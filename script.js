32b21d3bf08a14302731d84c1a9e2057c1472b2e
Unknown <jc6984@nyu.edu>
2019-07-24 12:11:10 -0400

    fixed front image breaking

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ function doParallax() {
     var layers = document.getElementsByClassName("parallax");
     var layer, speed, yPos;
     for (var i = 0; i < layers.length; i++) {
-      layer = layers[i];
-      speed = layer.getAttribute('parallax-speed');
-      var yPos = -(top * speed / 100);
-      layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        layer = layers[i];
+        speed = layer.getAttribute('parallax-speed');
+        if(speed != 100){
+            var yPos = -(top * speed / 100);
+            layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+        }else{
+            layer.setAttribute('style', 'position: relative');
+        }
 
     }
   });
