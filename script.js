e6e058a45c656c5accaac1cc888c08828b2aee22
Unknown <jc6984@nyu.edu>
2019-07-24 11:42:37 -0400

    the website stuff

---
+++ script.js
@@ -0,0 +1,19 @@
+function doParallax() {
+
+  window.addEventListener("scroll", function(event){
+
+    var top = this.pageYOffset;
+
+    var layers = document.getElementsByClassName("parallax");
+    var layer, speed, yPos;
+    for (var i = 0; i < layers.length; i++) {
+      layer = layers[i];
+      speed = layer.getAttribute('parallax-speed');
+      var yPos = -(top * speed / 100);
+      layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+
+    }
+  });
+}
+
+document.body.onload =doParallax();
