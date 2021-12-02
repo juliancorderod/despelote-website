dfc7fa16e8ddbdd867d531ebfa5358c36f6e0ca2
juliancorderod <jc6984@nyu.edu>
2021-12-02 01:45:03 -0500

    changed main description + added fancy new slideshow with a bunch of pictures!

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function doParallax() {
 
-  window.addEventListener("scroll", function(event){
+  window.addEventListener("scroll", function (event) {
 
     var top = this.pageYOffset;
 
     var layers = document.getElementsByClassName("parallax");
     var layer, speed, yPos;
     for (var i = 0; i < layers.length; i++) {
-        layer = layers[i];
-        speed = layer.getAttribute('parallax-speed');
-        if(speed != 100){
-            var yPos = -(top * speed / 100);
-            layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
-        }else{
-            layer.setAttribute('style', 'position: relative');
-        }
+      layer = layers[i];
+      speed = layer.getAttribute('parallax-speed');
+      if (speed != 100) {
+        var yPos = -(top * speed / 100);
+        layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+      } else {
+        layer.setAttribute('style', 'position: relative');
+      }
 
     }
   });
 }
 
-document.body.onload =doParallax();
+document.body.onload = doParallax();
